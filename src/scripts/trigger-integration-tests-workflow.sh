56445175625023aad9eaabf343bbf5c51c9d3f53
Gabriel Simmer <gabriel@gitgalaxy.com>
2020-07-15 12:55:39 +0100

    Debugging missing VCS_TYPE.

--- src/scripts/trigger-integration-tests-workflow.sh
+++ src/scripts/trigger-integration-tests-workflow.sh
@@ -1,8 +1,10 @@
 VCS_TYPE=$(echo "${CIRCLE_BUILD_URL}" | cut -d '/' -f 4)
+
 curl -u "${TOKEN}": -X POST --header "Content-Type: application/json" -d "{
     \"branch\": \"${CIRCLE_BRANCH}\",
     \"parameters\": \"${PARAM_MAP}\"
 }" "https://circleci.com/api/v2/project/${VCS_TYPE}/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/pipeline" -o /tmp/curl-result.txt
+
 CURL_RESULT=$(cat /tmp/curl-result.txt)
 
 if [[ $(echo "$CURL_RESULT" | jq -r .message) == "Not Found" || $(echo "$CURL_RESULT" | jq -r .message) == "Permission denied" || $(echo "$CURL_RESULT" | jq -r .message) == "Project not found" ]]; then
