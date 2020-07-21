1a66821efd69292640e678ddc298c6eadb889c9b
Kyle Tryon <Kyle.Tryon@outlook.com>
2020-07-20 23:14:58 -0400

    add conditional sudo to bats install

--- src/scripts/install-bats.sh
+++ src/scripts/install-bats.sh
@@ -3,10 +3,12 @@ if ! which git > /dev/null; then
     exit 1
 fi
 
+if [[ $EUID == 0 ]]; then export SUDO=""; else export SUDO="sudo"; fi
+
 cd /tmp || echo "Unable to open /tmp" && exit 1
 git clone https://github.com/bats-core/bats-core.git
 cd bats-core ||  echo "Failed to open cloned directory" && exit 1
-./install.sh /usr/local
+$SUDO ./install.sh /usr/local
 if ! which bats > /dev/null; then
     echo "BATS has failed to install."
     exit 1
