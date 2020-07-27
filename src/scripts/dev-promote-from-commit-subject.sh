5a6e56198d03541ee573362e9c9b89e184707f04
Kyle T <33272306+KyleTryon@users.noreply.github.com>
2020-07-27 10:27:03 -0400

    Orb 10 native tests (#94)
    Native testing for orb tools. will docs

--- src/scripts/dev-promote-from-commit-subject.sh
+++ src/scripts/dev-promote-from-commit-subject.sh
@@ -42,4 +42,8 @@ Main() {
     CheckIncrement
 }
 
-Main
+# Will not run if sourced from another script. This is done so this script may be tested.
+# View src/tests for more information.
+if [[ "$_" == "$0" ]]; then
+    Main
+fi
