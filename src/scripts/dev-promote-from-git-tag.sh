1255e9f55c5231415e90185b1264eaeb7d0828ff
Gabriel Simmer <gabriel@gitgalaxy.com>
2020-08-18 09:51:04 +0100

    Additional functions over monolithic files.

--- src/scripts/dev-promote-from-git-tag.sh
+++ src/scripts/dev-promote-from-git-tag.sh
@@ -1,21 +1,40 @@
-RELEASE_TYPE=''
-T=$(eval echo "$TOKEN")
-REF=$(eval echo "$ORB_REF")
+Setup() {
+    RELEASE_TYPE=''
+    T=$(eval echo "$TOKEN")
+    REF=$(eval echo "$ORB_REF")
+}
 
-if [[ "${CIRCLE_TAG}" =~ ${MAJOR_RELEASE_TAG_REGEX} ]]; then
-    RELEASE_TYPE='major'
-elif [[ "${CIRCLE_TAG}" =~ ${MINOR_RELEASE_TAG_REGEX} ]]; then
-    RELEASE_TYPE='minor'
-elif [[ "${CIRCLE_TAG}" =~ ${PATCH_RELEASE_TAG_REGEX} ]]; then
-    RELEASE_TYPE='patch'
-fi
-if [ -n "${RELEASE_TYPE}" ]; then
-    PUBLISH_MESSAGE=$(circleci orb publish promote \
-    "${ORB_NAME}@${REF}" \
-    "${RELEASE_TYPE}" --token \
-    "${T}" \
-    --skip-update-check)
-    echo "$PUBLISH_MESSAGE"
-    ORB_VERSION=$(echo "$PUBLISH_MESSAGE" | sed -n 's/Orb .* was promoted to `\(.*\)`.*/\1/p')
-    echo "export PR_MESSAGE=\"BotComment: *Production* version of orb available for use - \\\`${ORB_VERSION}\\\`\"" >> "$BASH_ENV"
+DiscoverTag() {
+    if [[ "${CIRCLE_TAG}" =~ ${MAJOR_RELEASE_TAG_REGEX} ]]; then
+        RELEASE_TYPE='major'
+    elif [[ "${CIRCLE_TAG}" =~ ${MINOR_RELEASE_TAG_REGEX} ]]; then
+        RELEASE_TYPE='minor'
+    elif [[ "${CIRCLE_TAG}" =~ ${PATCH_RELEASE_TAG_REGEX} ]]; then
+        RELEASE_TYPE='patch'
+    fi
+}
+
+PublishTag() {
+    if [ -n "${RELEASE_TYPE}" ]; then
+        PUBLISH_MESSAGE=$(circleci orb publish promote \
+        "${ORB_NAME}@${REF}" \
+        "${RELEASE_TYPE}" --token \
+        "${T}" \
+        --skip-update-check)
+        echo "$PUBLISH_MESSAGE"
+        ORB_VERSION=$(echo "$PUBLISH_MESSAGE" | sed -n 's/Orb .* was promoted to `\(.*\)`.*/\1/p')
+        echo "export PR_MESSAGE=\"BotComment: *Production* version of orb available for use - \\\`${ORB_VERSION}\\\`\"" >> "$BASH_ENV"
+    else
+        echo "Unable to determine semver bump from tag (${CIRCLE_TAG})."
+    fi
+}
+
+Main() {
+    Setup
+    DiscoverTag
+    PublishTag
+}
+
+if [[ "$_" == "$0" ]]; then
+    Main
 fi
--- src/scripts/trigger-integration-tests-workflow.sh
+++ src/scripts/trigger-integration-tests-workflow.sh
@@ -1,24 +1,31 @@
-VCS_TYPE=$(echo "${CIRCLE_BUILD_URL}" | cut -d '/' -f 4)
+Setup() {
+    VCS_TYPE=$(echo "${CIRCLE_BUILD_URL}" | cut -d '/' -f 4)
+    T=$(eval echo "$TOKEN")
+    PARAMS=$(eval echo "$PARAM_MAP")
+}
 
-T=$(eval echo "$TOKEN")
-PARAMS=$(eval echo "$PARAM_MAP")
 
-# Remove later
-echo "parameter map:"
-echo "{\"branch\": \"${CIRCLE_BRANCH}\",\"parameters\": ${PARAMS}}" > pipelineparams.json
-echo "Pipeline params:"
-cat pipelineparams.json
+DoCurl() {
+    curl -u "${T}": -X POST --header "Content-Type: application/json" -d @pipelineparams.json "https://circleci.com/api/v2/project/${VCS_TYPE}/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/pipeline" -o /tmp/curl-result.txt
+    CURL_RESULT=$(cat /tmp/curl-result.txt)
+}
 
-curl -u "${T}": -X POST --header "Content-Type: application/json" -d @pipelineparams.json "https://circleci.com/api/v2/project/${VCS_TYPE}/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/pipeline" -o /tmp/curl-result.txt
+Result() {
+    if [[ $(echo "$CURL_RESULT" | jq -r .message) == "Not Found" || $(echo "$CURL_RESULT" | jq -r .message) == "Permission denied" || $(echo "$CURL_RESULT" | jq -r .message) == "Project not found" ]]; then
+        echo "Was unable to trigger integration test workflow. API response: $(cat /tmp/curl-result.txt | jq -r .message)"
+        exit 1
+    else
+        echo "Pipeline triggered!"
+        echo "https://app.circleci.com/jobs/${VCS_TYPE}/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/$(cat /tmp/curl-result.txt | jq -r .number)"
+    fi
+}
 
-echo "Result $(cat /tmp/curl-result.txt)"
+Main() {
+    Setup
+    DoCurl
+    Result
+}
 
-CURL_RESULT=$(cat /tmp/curl-result.txt)
-
-if [[ $(echo "$CURL_RESULT" | jq -r .message) == "Not Found" || $(echo "$CURL_RESULT" | jq -r .message) == "Permission denied" || $(echo "$CURL_RESULT" | jq -r .message) == "Project not found" ]]; then
-    echo "Was unable to trigger integration test workflow. API response: $(cat /tmp/curl-result.txt | jq -r .message)"
-    exit 1
-else
-    echo "Pipeline triggered!"
-    echo "https://app.circleci.com/jobs/${VCS_TYPE}/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/$(cat /tmp/curl-result.txt | jq -r .number)"
+if [[ "$_" == "$0" ]]; then
+    Main
 fi
