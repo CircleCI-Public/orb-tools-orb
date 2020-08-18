519bcf6e07d3922558d66ce258c68ba3295dc4bf
Gabriel Simmer <gabriel@gitgalaxy.com>
2020-08-18 10:35:58 +0100

    Fix workflow trigger.

--- src/scripts/trigger-integration-tests-workflow.sh
+++ src/scripts/trigger-integration-tests-workflow.sh
@@ -1,15 +1,16 @@
 Setup() {
     VCS_TYPE=$(echo "${CIRCLE_BUILD_URL}" | cut -d '/' -f 4)
     T=$(eval echo "$TOKEN")
+    echo $PARAM_MAP > pipelineparams.json
 }
 
-
 DoCurl() {
-    curl -u "${T}": -X POST --header "Content-Type: application/json" -d @pipelineparams.json "https://circleci.com/api/v2/project/${VCS_TYPE}/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/pipeline" -o /tmp/curl-result.txt
-    CURL_RESULT=$(cat /tmp/curl-result.txt)
+    curl -u "${T}": -X POST --header "Content-Type: application/json" -d @pipelineparams.json \
+      "https://circleci.com/api/v2/project/${VCS_TYPE}/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/pipeline" -o /tmp/curl-result.txt
 }
 
 Result() {
+    CURL_RESULT=$(cat /tmp/curl-result.txt)
     if [[ $(echo "$CURL_RESULT" | jq -r .message) == "Not Found" || $(echo "$CURL_RESULT" | jq -r .message) == "Permission denied" || $(echo "$CURL_RESULT" | jq -r .message) == "Project not found" ]]; then
         echo "Was unable to trigger integration test workflow. API response: $(cat /tmp/curl-result.txt | jq -r .message)"
         exit 1
