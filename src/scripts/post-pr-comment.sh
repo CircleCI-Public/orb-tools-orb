6b770117d732edd7f36fdd92d596ccab60703165
Kyle Tryon <Kyle.Tryon@outlook.com>
2021-05-17 21:49:17 -0400

    initialize v11 scaffolding

--- src/scripts/post-pr-comment.sh
+++
@@ -1,7 +0,0 @@
-PR_NUMBER=$(git log -1 --pretty=%s. | sed -n "$SED_EXP")
-echo "PR_NUMBER is ${PR_NUMBER}"
-if [ "$PR_NUMBER" == "" ];then
-    echo "No pr found; do nothing. If this is a mistake, check if your PR commit message matches the $SED_EXP sed expression."
-    exit 0
-fi
-curl -X POST -u "${BOT_USER}:${BOT_TOKEN}" "https://api.github.com/repos/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/issues/${PR_NUMBER}/comments" -d "{\"body\":\"${COMMENT}\"}"
