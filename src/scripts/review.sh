06ce5288e944458aae22a50e20aeb8ff9eb5458d
Kyle Tryon <Kyle.Tryon@outlook.com>
2021-05-18 12:32:58 -0400

    fix pack

--- src/scripts/review.sh
+++ src/scripts/review.sh
@@ -8,4 +8,6 @@ if ! command -v COMMAND &> /dev/null; then
 fi
 mkdir -p /tmp/orb_review
 echo "$ORB_REVIEW_BATS_FILE "> review.bats
+echo "Reviewing orb best practices"
+echo "If required, tests can be skipped via their \"RCXXX\" code with the \"exclude\" parameter."
 bats --tap ./review.bats
