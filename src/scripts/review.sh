67ddd9fffb310a7cd870e78a0ce77172ebabf01a
KyleTryon <Kyle.Tryon@circleci.com>
2022-02-16 00:15:05 -0500

    chore: update review job image and output

--- src/scripts/review.sh
+++ src/scripts/review.sh
@@ -3,11 +3,15 @@ if ! command -v bats; then
 	echo 'The "bats-core" automation framework must be installed to execute review testing.'
 	exit 1
 fi
-if ! command -v COMMAND &> /dev/null; then
-	pip3 install yq -q
+if ! command -v yq ; then
+	echo 'The "yq" package must be installed to execute review testing.'
+	exit 1
 fi
 mkdir -p /tmp/orb_review
 echo "$ORB_REVIEW_BATS_FILE "> review.bats
 echo "Reviewing orb best practices"
 echo "If required, tests can be skipped via their \"RCXXX\" code with the \"exclude\" parameter."
 bats --tap ./review.bats
+
+echo "Review complete!"
+echo "You can review the suggestions above. If you would like to ignore any of the suggestions, add their \"RCXXX\" code to the \"exclude\" parameter."
