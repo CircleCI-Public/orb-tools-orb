2094588f7661647402d2e6206bfde7a8b5d9a437
KyleTryon <Kyle.Tryon@circleci.com>
2022-03-08 12:20:15 -0500

    chore: formatting

--- src/scripts/review.sh
+++ src/scripts/review.sh
@@ -14,11 +14,11 @@ if ! command -v yq >/dev/null; then
 fi
 
 mkdir -p /tmp/orb_dev_kit/review/
-echo "$ORB_REVIEW_BATS_FILE " >review.bats
+echo "$ORB_REVIEW_BATS_FILE" > review.bats
 echo "Reviewing orb best practices"
 echo "If required, tests can be skipped via their \"RCXXX\" code with the \"exclude\" parameter."
 EXIT_CODE=0
 bats -T --formatter junit ./review.bats >/tmp/orb_dev_kit/review/review.xml || EXIT_CODE=$?
 echo "Review complete!"
 echo "Review the results in the \"TESTS\" tab above. If you would like to ignore any of the suggestions, add their \"RCXXX\" code to the \"exclude\" parameter."
-exit $EXIT_CODE
+exit "$EXIT_CODE"
