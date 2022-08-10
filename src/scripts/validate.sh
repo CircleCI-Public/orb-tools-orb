01bea29e976719d7622afb60b3f9aced7ecd087c
Kelvin Tay <kelvintaywl@gmail.com>
2022-08-11 08:06:56 +0900

    Update src/scripts/validate.sh
    Co-authored-by: Kyle T <33272306+KyleTryon@users.noreply.github.com>

--- src/scripts/validate.sh
+++ src/scripts/validate.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-circleci orb validate --host "${CIRCLE_API_HOST}" --token "${CIRCLE_TOKEN}" --skip-update-check "${ORB_PARAM_OUTPUT_DIR}orb.yml"
+circleci orb validate --host "${CIRCLECI_API_HOST}" --token "${CIRCLE_TOKEN}" --skip-update-check "${ORB_PARAM_OUTPUT_DIR}orb.yml"
