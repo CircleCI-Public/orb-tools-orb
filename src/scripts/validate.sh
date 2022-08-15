47542d19027bd1b2068a567ba48bc6a3990fe1be
kelvintaywl <kelvintaywl@gmail.com>
2022-08-15 16:34:14 +0900

    feat: verify CIRCLE_TOKEN is set for self-hosted CircleCI when pack

--- src/scripts/validate.sh
+++ src/scripts/validate.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-circleci orb validate --host "${CIRCLECI_API_HOST}" --token "${CIRCLE_TOKEN}" --skip-update-check "${ORB_PARAM_OUTPUT_DIR}orb.yml"
+# NOTE: when validating against CircleCI Cloud, no explicit API Token is needed.
+# Hence, the --token option here falls back to a dummy value
+circleci orb validate --host "${CIRCLECI_API_HOST:-}" --token "${CIRCLE_TOKEN:-dummy}" --skip-update-check "${ORB_PARAM_OUTPUT_DIR}orb.yml"
