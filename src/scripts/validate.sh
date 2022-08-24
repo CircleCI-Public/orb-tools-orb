0e7b77bfb950dd88320474a446a677470abb040c
kelvintaywl <kelvintaywl@gmail.com>
2022-08-24 16:15:40 +0900

    refactor: mv token check within validate.sh

--- src/scripts/validate.sh
+++ src/scripts/validate.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-# NOTE: when validating against CircleCI Cloud, no explicit API Token is needed.
-# Hence, the --token option here falls back to a dummy value
+# NOTE: an explicit API token is required for orb validation, for self-hosted CircleCI.
+# In the case of CircleCI cloud (https://circleci.com), an API token is not needed.
+if [ "https://circleci.com" != "${CIRCLECI_API_HOST}" ] && [ -z "${CIRCLE_TOKEN}" ]; then
+    echo "Please set a valid CIRCLE_TOKEN token from your self-hosted CircleCI."
+    exit 1
+fi
+
 circleci orb validate --host "${CIRCLECI_API_HOST:-}" --token "${CIRCLE_TOKEN:-dummy}" --skip-update-check "${ORB_PARAM_OUTPUT_DIR}orb.yml"
