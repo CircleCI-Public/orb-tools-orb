f6b29b74eccc7ea8a32013b49d6adf34551636e8
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-12-08 15:40:19 -0800

    fix: add missing `$`

--- src/scripts/publish.sh
+++ src/scripts/publish.sh
@@ -53,7 +53,7 @@ function orbPublish() {
       exit 0
     fi
     validateProdTag
-    ORB_RELEASE_VERSION="$(printf '%s' "CIRCLE_TAG" | grep -o -E '[0-9]+\.[0-9]+\.[0-9]+')"
+    ORB_RELEASE_VERSION="$(printf '%s' "$CIRCLE_TAG" | grep -o -E '[0-9]+\.[0-9]+\.[0-9]+')"
     echo "Production version: ${ORB_RELEASE_VERSION}"
     printf "\n"
     publishOrb "${ORB_RELEASE_VERSION}"
