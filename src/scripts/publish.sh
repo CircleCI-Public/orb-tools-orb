79d63a8d94b1a0580a4ca2d83eb19d0a4a982cdf
KyleTryon <git@techsquidtv.com>
2023-05-02 10:25:40 -0400

    refactor: replace echo with printf

--- src/scripts/publish.sh
+++ src/scripts/publish.sh
@@ -5,21 +5,20 @@ ORB_FILE=${ORB_PARAM_ORB_OUTPUT_FILE#/}
 
 function validateProdTag() {
   if [[ ! "${CIRCLE_TAG}" =~ $ORB_PARAM_TAG_PATTERN ]]; then
-    echo "Malformed tag detected."
-    echo "Tag: $CIRCLE_TAG"
-    echo
-    echo "A production release has attempted to occur, but the tag does not match the expected pattern."
-    echo "Aborting deployment. Push a new tag with the compatible form."
-    echo "Current tag pattern: $ORB_PARAM_TAG_PATTERN"
+    printf "Malformed tag detected.\n"
+    printf "Tag: %s\n\n" "$CIRCLE_TAG"
+    printf "A production release has attempted to occur, but the tag does not match the expected pattern.\n"
+    printf "Aborting deployment. Push a new tag with the compatible form.\n"
+    printf "Current tag pattern: %s\n" "$ORB_PARAM_TAG_PATTERN"
     exit 1
   fi
 }
 
 function validateOrbPubToken() {
   if [[ -z "${ORB_PARAM_ORB_PUB_TOKEN}" ]]; then
-    echo "No Orb Publishing Token detected."
-    echo "Please set the CIRCLE_TOKEN environment variable."
-    echo "Aborting deployment."
+    printf "No Orb Publishing Token detected.\n"
+    printf "Please set the CIRCLE_TOKEN environment variable.\n"
+    printf "Aborting deployment.\n"
     exit 1
   fi
 }
@@ -29,9 +28,9 @@ function publishOrb() {
   circleci orb publish --host "${CIRCLECI_API_HOST:-https://circleci.com}" --skip-update-check "${ORB_DIR}/${ORB_FILE}" "${ORB_PARAM_ORB_NAME}@${1}" --token "$ORB_PARAM_ORB_PUB_TOKEN"
   printf "\n"
   {
-    echo "Your orb has been published to the CircleCI Orb Registry."
-    echo "You can view your published orb on the CircleCI Orb Registry at the following link: "
-    echo "https://circleci.com/developer/orbs/orb/${ORB_PARAM_ORB_NAME}?version=${1}"
+    printf "Your orb has been published to the CircleCI Orb Registry.\n"
+    printf "You can view your published orb on the CircleCI Orb Registry at the following link: \n"
+    printf "https://circleci.com/developer/orbs/orb/%s?version=%s\n" "${ORB_PARAM_ORB_NAME}" "${1}"
   } >/tmp/orb_dev_kit/publishing_message.txt
 }
 
@@ -39,40 +38,38 @@ function publishDevOrbs() {
   publishOrb "dev:${CIRCLE_SHA1}"
   publishOrb "dev:alpha"
   {
-    echo "Your development orb has been published. It will expire in 30 days."
-    echo "You can preview what this will look like on the CircleCI Orb Registry at the following link: "
-    echo "https://circleci.com/developer/orbs/orb/${ORB_PARAM_ORB_NAME}?version=dev:${CIRCLE_SHA1}"
+    printf "Your development orb has been published. It will expire in 30 days.\n"
+    printf "You can preview what this will look like on the CircleCI Orb Registry at the following link: \n"
+    printf "https://circleci.com/developer/orbs/orb/%s?version=dev:%s\n" "${ORB_PARAM_ORB_NAME}" "${CIRCLE_SHA1}"
   } >/tmp/orb_dev_kit/publishing_message.txt
 }
 
 # The main function
 function orbPublish() {
-  echo "Preparing to publish your orb."
+  printf "Preparing to publish your orb.\n"
   validateOrbPubToken
 
   if [ "$ORB_PARAM_PUB_TYPE" == "production" ]; then
-    echo "Production release detected!"
+    printf "Production release detected!\n"
     if [ -z "$CIRCLE_TAG" ]; then
-      echo "No tag detected. Peacfully exiting."
-      echo "If you are trying to publish a production orb, you must push a semantically versioned tag."
+      printf "No tag detected. Peacfully exiting.\n"
+      printf "If you are trying to publish a production orb, you must push a semantically versioned tag.\n"
       exit 0
     fi
     validateProdTag
     ORB_RELEASE_VERSION="$(echo "${CIRCLE_TAG}" | grep -Eo "[0-9]+\.[0-9]+\.[0-9]+")"
-    echo "Production version: ${ORB_RELEASE_VERSION}"
-    printf "\n"
+    printf "Production version: %s\n\n" "${ORB_RELEASE_VERSION}"
     publishOrb "${ORB_RELEASE_VERSION}"
   elif [ "$ORB_PARAM_PUB_TYPE" == "dev" ]; then
-    echo "Development release detected!"
-    printf "\n"
+    printf "Development release detected!\n\n"
     publishDevOrbs
   else
-    echo "No release type detected."
-    echo "Please report this error."
+    printf "No release type detected.\n"
+    printf "Please report this error.\n"
   fi
 
   printf "\n\n"
-  echo "********************************************************************************"
+  printf "********************************************************************************\n"
   cat /tmp/orb_dev_kit/publishing_message.txt
 
 }
