3e60f65a7724ce4f7a7186612b9643d691c13d76
Rosie Yohannan <rosie@circleci.com>
2025-12-10 12:26:21 +0000

    Fix docs link

--- src/scripts/lint.sh
+++ src/scripts/lint.sh
@@ -2,7 +2,7 @@
 if [ ! -d "$ORB_VAL_SOURCE_DIR" ]; then
     printf "No source directory located at %s\n" "$ORB_VAL_SOURCE_DIR"
     printf "This orb assumes you have built your orb using the Orb Development Kit\n"
-    printf "https://circleci.com/docs/2.0/orb-author/#orb-development-kit\n"
+    printf "https://circleci.com/docs/orbs/author/orb-development-kit/\n"
     exit 1
 fi
 pip install --user yamllint
